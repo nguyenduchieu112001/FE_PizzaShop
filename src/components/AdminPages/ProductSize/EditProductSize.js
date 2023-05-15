7db62bf585e7fe0d35b4277f51b111f3f16a250d
nguyenduchieu112001 <nguyenduchieu112001@gmail.com>
2023-05-15 16:47:03 +0700

    change UI of EditProductSize

--- src/components/AdminPages/ProductSize/EditProductSize.js
+++ src/components/AdminPages/ProductSize/EditProductSize.js
@@ -1,32 +1,43 @@
-import React, { useState } from 'react'
-import { Button, Form, FormControl, FormGroup } from 'react-bootstrap';
-import { Label } from 'semantic-ui-react';
+import React, { useState } from "react";
+import { Button, Form, FormControl, FormGroup } from "react-bootstrap";
+import { Label } from "semantic-ui-react";
 
-function EditProductSize({productSize, handleEdit}) {
-    const [name, setName] = useState(productSize.name);
-    const handleSave = async (e) => {
-        e.preventDefault();
-        const form = {
-          name,
-        };
-        handleEdit(form);
-      };
-      return (
-        <Form onSubmit={handleSave}>
-            <FormGroup controlId="productTypeName">
-            <Label>Product Type Name</Label>
-            <FormControl
-              type="text"
-              placeholder="Enter product type name"
-              value={name}
-              onChange={(e) => setName(e.target.value)}
-            />
-          </FormGroup>
-          <Button variant="primary" type="submit">
-            Save Changes
-          </Button>
-        </Form>
-      );
+function EditProductSize({ productSize, handleEdit }) {
+  const [name, setName] = useState(productSize.name);
+  const [percent, setPercent] = useState(productSize.percentPrice);
+  const handleSave = async (e) => {
+    e.preventDefault();
+    const form = {
+      name,
+      percentPrice: percent,
+    };
+    handleEdit(form);
+  };
+  return (
+    <Form onSubmit={handleSave}>
+      <FormGroup controlId="productSizeName">
+        <Label>Product Size Name</Label>
+        <FormControl
+          type="text"
+          placeholder="Enter product type name"
+          value={name}
+          onChange={(e) => setName(e.target.value)}
+        />
+      </FormGroup>
+      <FormGroup controlId="productTypeName">
+        <Label>Product Size Percent</Label>
+        <FormControl
+          type="number"
+          placeholder="Enter product type name"
+          value={percent}
+          onChange={(e) => setPercent(e.target.value)}
+        />
+      </FormGroup>
+      <Button variant="primary" type="submit">
+        Save Changes
+      </Button>
+    </Form>
+  );
 }
 
-export default EditProductSize
+export default EditProductSize;
